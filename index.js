0c2cb9cfd47281ba9f9361867cf673a945eaafd4
Md Antor <md.antor1155@gmail.com>
2022-05-08 15:27:08 +0800

    api for home page done

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
-const {MongoClient} = require('mongodb');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
 //cross origin resource sharing
 const cors = require('cors');
@@ -10,30 +11,47 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 
-//connection mongodb
-const uri = "mongodb+srv://mdAntor:h7HlxzP7pI28SF3M@cluster0.js1z3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+//declearing mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.js1z3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
 
+
+// working with api and data collection 
 async function run() {
   try {
     await client.connect();
-
     const productCollection = client.db("warehouseOfFoodCompany").collection("Products");
- 
 
-    // create a document to insert
-    const doc = {
-      title: "Record of a Shriveled Datum",
-      content: "No bytes, no problem. Just insert a document, in MongoDB",
-    }
+/// for home page
+    app.get('/home', async(req,res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
 
-    // const result = await haiku.insertOne(doc);
-    // console.log(`A document was inserted with the _id: ${result.insertedId}`);
-    // console.log('result is', result);
+      const products = await cursor.limit(6).toArray();
 
-  }  finally {
+      console.log(products.length);
+      res.send(products);
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
   }
 }
 run().catch(console.dir);
@@ -41,9 +59,9 @@ run().catch(console.dir);
 
 
 
-app.get('/', (req, res)=>{
-    res.send('running from server');
+app.get('/', (req, res) => {
+  res.send('running from server');
 })
-app.listen(port,()=>{
-    console.log('runnig server')
+app.listen(port, () => {
+  console.log('runnig server')
 })
